eee2191eecac77d42ccd18c88800ff5f476ea7ce
Félix Laguna Teno <lagunatenofelix@gmail.com>
2019-09-24 14:43:29 +0200

    Debuging

--- src/Rcommand_Sreport.c
+++ src/Rcommand_Sreport.c
@@ -91,6 +91,7 @@ char *command_Sreport(char **new_dir_ptr){
     fprintf(stderr,"Null pointer!!");
   }else{
     fprintf(stderr,"NOT a Null pointer!!\n");
+    fprintf(stderr,"%c\n",new_dir[0]);
     size_t l=strlen(new_dir);
     fprintf(stderr,"length: %d\n", l);
     fprintf(stderr,"errno: %d\n", errno);
